b51689f4a7e831a279174c37076c93102f1aa891
pyroserpent <zacafnan@gmail.com>
2023-12-19 14:40:53 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 const PORT = 3001;
-
+const cors = require('cors');
+app.use(cors());
 app.use(express.json()); // Middleware for parsing JSON bodies
 
 // Temporary in-memory storage for quotes
