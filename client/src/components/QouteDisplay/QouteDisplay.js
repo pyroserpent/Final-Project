c5f2fc4e6df337db895e0678c963d9cf4439881d
pyroserpent <zacafnan@gmail.com>
2023-12-19 13:20:21 -0500

    Js added

---
+++ client/src/components/QouteDisplay/QouteDisplay.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './QuoteDisplay.css';
+
+const QuoteDisplay = ({ quote, author }) => (
+  <div className="quote-container">
+    <div className="quote-text">{quote}</div>
+    <div className="quote-author">- {author}</div>
+  </div>
+);
+
+export default QuoteDisplay;
