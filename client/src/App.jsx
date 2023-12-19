d2ae62efd172b6bb9406624b9bd66cd5424e059a
pyroserpent <zacafnan@gmail.com>
2023-12-19 16:34:41 -0500

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -35,11 +35,11 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Quote Of the Day
         </p>
       </div>
 
-      {/*  */}
+      {/* Created by */}
       <p className="read-the-docs">
         Created by Ziaul Chowdhury
       </p>
