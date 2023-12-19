2d0e5b2439646d273af31a5def5c48c196c8d1f4
pyroserpent <zacafnan@gmail.com>
2023-12-19 14:37:49 -0500

    update cors

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 const PORT = 3001;
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
 
 app.use(express.json()); // Middleware for parsing JSON bodies
 
