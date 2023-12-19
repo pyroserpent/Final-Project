70a8539a65f308f43e52638f824b54aa16f23f10
pyroserpent <zacafnan@gmail.com>
2023-12-19 13:58:56 -0500

    Qoute Form separated in folder

---
+++ client/src/components/QuoteForm/QuoteForm.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import './QuoteForm.css';
+
+
+const QuoteForm = () => {
+  const [quote, setQuote] = useState('');
+  const [author, setAuthor] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // POST request to the server with the new quote
+    fetch('http://localhost:3001/quote', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ quote, author }),
+    })
+    .then(response => response.json())
+    .then(data => {
+      alert('Quote submitted successfully!');
+      // Reset the form fields
+      setQuote('');
+      setAuthor('');
+    })
+    .catch(error => {
+      console.error('Error submitting quote:', error);
+    });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="quote">Quote</label>
+        <textarea 
+          id="quote" 
+          value={quote} 
+          onChange={(e) => setQuote(e.target.value)} 
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="author">Author</label>
+        <input 
+          type="text" 
+          id="author" 
+          value={author} 
+          onChange={(e) => setAuthor(e.target.value)} 
+          required
+        />
+      </div>
+      <button type="submit">Submit Quote</button>
+    </form>
+  );
+};
+
+export default QuoteForm;
