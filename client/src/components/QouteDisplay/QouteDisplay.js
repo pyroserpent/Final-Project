e37eb636977eb694b64f598a347da68721e92822
pyroserpent <zacafnan@gmail.com>
2023-12-19 13:29:57 -0500

    UseEffect added

--- client/src/components/QouteDisplay/QouteDisplay.js
+++ client/src/components/QouteDisplay/QouteDisplay.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './QuoteDisplay.css';
 
-const QuoteDisplay = ({ quote, author }) => (
-  <div className="quote-container">
-    <div className="quote-text">{quote}</div>
-    <div className="quote-author">- {author}</div>
-  </div>
-);
+const QuoteDisplay = () => {
+  const [quoteData, setQuoteData] = useState({ quote: '', author: '' });
+
+  useEffect(() => {
+    // Fetch the quote from the server
+    fetch('http://localhost:3001/quote')
+      .then(response => response.json())
+      .then(data => {
+        setQuoteData(data);
+      })
+      .catch(error => {
+        console.error('Error fetching quote:', error);
+        // Handle error or set default message
+        setQuoteData({ quote: 'Error loading quote.', author: '' });
+      });
+  }, []);
+
+  return (
+    <div className="quote-container">
+      <div className="quote-text">{quoteData.quote}</div>
+      <div className="quote-author">- {quoteData.author}</div>
+    </div>
+  );
+};
 
 export default QuoteDisplay;
