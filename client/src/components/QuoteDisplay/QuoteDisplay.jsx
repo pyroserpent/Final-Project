e82a743e4554998d6648878d5f30536e1ed1d1a5
pyroserpent <zacafnan@gmail.com>
2023-12-19 14:27:26 -0500

    Update QuoteDisplay.jsx

--- client/src/components/QuoteDisplay/QuoteDisplay.jsx
+++ client/src/components/QuoteDisplay/QuoteDisplay.jsx
@@ -1,27 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './QuoteDisplay.css';
 
-const QuoteDisplay = () => {
-  const [quoteData, setQuoteData] = useState({ quote: '', author: '' });
-
-  useEffect(() => {
-    // Fetch the quote from the server
-    fetch('http://localhost:3001/quote')
-      .then(response => response.json())
-      .then(data => {
-        setQuoteData(data);
-      })
-      .catch(error => {
-        console.error('Error fetching quote:', error);
-        // Handle error or set default message
-        setQuoteData({ quote: 'Error loading quote.', author: '' });
-      });
-  }, []);
-
+const QuoteDisplay = ({ quote, author }) => {
   return (
     <div className="quote-container">
-      <div className="quote-text">{quoteData.quote}</div>
-      <div className="quote-author">- {quoteData.author}</div>
+      <div className="quote-text">{quote}</div>
+      <div className="quote-author">- {author}</div>
     </div>
   );
 };
